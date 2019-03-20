0b54d30e3c882a2089e90c2e6967755b9b81532a
Brinda Earnest <earnes2@mail.nmc.edu>
2019-03-20 13:56:11 -0400

    Initial push

---
+++ arrayFruitEnums/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayFruitEnums
+{
+    class Program
+    {
+        public enum Fruit
+        {
+            unavailable, melon, strawberry, apple, kiwi, pineapple, tomato
+        }
+
+        static void Main(string[] args)
+        {
+            Fruit[] fruits = new Fruit[3]; //3 is the number of elements in the array
+            string userResponse;
+
+
+            for (int index = 0; index < 3; index++)
+            {
+
+                Console.WriteLine($"Enter Fruit choice #{index + 1}: ");
+                userResponse = Console.ReadLine();
+                //we want to parse the response into the enum, and put that response into the index of the array
+                Enum.TryParse(userResponse, out fruits[index]);
+                
+            }
+            Console.WriteLine();
+
+            foreach (Fruit fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
+
+
